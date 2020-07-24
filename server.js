a3870c02b4f82686ffa47a510e2ec6696a0a7cf5
borrikon <borrikon@gmail.com>
2020-07-24 14:26:08 +0300

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(express.static(_dirname + './dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    app.use(express.static(__dirname + '/dist/angular-news'));
+    res.sendFile(path.join(__dirname + '/dist/angular-news/index.html'));
 })
 
 console.log('Console listening!')
